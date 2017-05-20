14ed08ba4563c19cc4e641f33aa5971b6e955b2e
Mike <velddev@users.noreply.github.com>
2017-05-21 00:20:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res)
+{
+	res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket)
+{
   console.log('a user connected');
+	
+	socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+	});
 });
 
-http.listen(3000, function(){
+http.listen(3000, function()
+{
   console.log('listening on *:3000');
 });
