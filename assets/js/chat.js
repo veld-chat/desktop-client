a41870a22f72804ddde1eafa30abe9470ab4bc04
Mike <veldsinkie.mv@gmail.com>
2017-05-21 21:48:54 +0200

    add: messagebuilders

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,19 +1,16 @@
 var messageContainer;
 var inputWindow;
 
-var socket = new io();
-
-socket.on('usr-msg', function(msg) {
-    messageContainer.innerHTML += CreateMessage("anon", msg);
-});
-
-socket.on('sys-msg', function(msg) {
-    messageContainer.innerHTML += CreateSystemMessage(msg);
-});
+//*
+    var socket = new io("37.97.187.157:1234");
+/*/
+    var socket = new io();
+//*/
 
 function Init() 
 {
     messageContainer = document.getElementById("msg-container");
+    console.log(messageContainer);
     if(messageContainer === null) 
     {
         alert("error: messageContainer seems to not have been found?!");
@@ -32,25 +29,69 @@ function Init()
     };
 }
 
+socket.on('usr-msg', function(msg) {
+    messageContainer.appendChild(CreateMessage(msg));
+    messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+});
+
+socket.on('sys-msg', function(msg) {
+    messageContainer.appendChild(CreateSystemMessage(msg));
+    messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+});
+
 function TrySendMessage(e) 
 {
     var charCode = e.keyCode;
 
     if (charCode == 13) 
     {
-        socket.emit('usr-msg', inputWindow.value);
-        inputWindow.value = "";
+        if(inputWindow.value.trim() != "")
+        {
+            socket.emit('usr-msg', inputWindow.value);
+            inputWindow.value = "";
+        }
     }
 }
 
-function CreateMessage(name, msg)
+function CreateMessage(msg)
 {
-    return "<ul class='msg-instance-container'><div class='msg-instance-avatar'></div><div class='msg-instance-title'>" + name + "</div><div class='msg-instance'>" + msg + "</div></ul>";
+    var messageInstanceContainer = document.createElement("ul");
+    messageInstanceContainer.className = "msg-instance-container fadein";
+
+    var messageInstanceAvatar = document.createElement("div");
+    messageInstanceAvatar.className = "msg-instance-avatar";
+    messageInstanceAvatar.style = "background: url('" + msg.avatarurl + "'); background-position: center; background-size: contain;";
+    messageInstanceContainer.appendChild(messageInstanceAvatar);
+
+    var messageInstanceTitle = document.createElement("div");
+    messageInstanceTitle.className = "msg-instance-title";
+    messageInstanceTitle.textContent = msg.name;
+    messageInstanceContainer.appendChild(messageInstanceTitle);
+
+    var messageInstance = document.createElement("div");
+    messageInstance.className = "msg-instance";
+    messageInstance.textContent = msg.message;
+    messageInstanceContainer.appendChild(messageInstance);
+
+    return messageInstanceContainer;
 }
 
 function CreateSystemMessage(msg)
 {
-    return "<ul class='msg-instance-container'><div class='msg-instance-title'>system</div><div class='msg-instance'>" + msg + "</div></ul>";
+    var messageInstanceContainer = document.createElement("ul");
+    messageInstanceContainer.className = "msg-instance-container fadein";
+
+    var messageInstanceTitle = document.createElement("div");
+    messageInstanceTitle.className = "msg-instance-title";
+    messageInstanceTitle.textContent = "system";
+    messageInstanceContainer.appendChild(messageInstanceTitle);
+
+    var messageInstance = document.createElement("div");
+    messageInstance.className = "msg-instance";
+    messageInstance.textContent = msg;
+    messageInstanceContainer.appendChild(messageInstance);
+
+    return messageInstanceContainer;
 }
 
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+let clients = {};
 
 app.get('/', function(req, res)
 {
@@ -14,16 +16,57 @@ app.use("/assets/js", express.static(__dirname + '/assets/js'));
 
 io.on('connection', function(socket)
 {
-	console.log('a user connected');
-   	io.emit('sys-msg', "A new user has connected");
-	
+	clients[socket.id] = {};
+	clients[socket.id].name = "anon-" + socket.id;
+	clients[socket.id].socket = socket;
+	clients[socket.id].avatarid = Math.floor(Math.random() * 70) + 1;
+	clients[socket.id].avatarurl = "https://randomuser.me/api/portraits/men/" + clients[socket.id].avatarid + ".jpg";
+	clients[socket.id].namecolor = "black";
+
+	console.log('user- connected');
+   	io.emit('sys-msg', clients[socket.id].name + " has joined.");
+
+   	socket.on("disconnect", function()
+   	{
+   		console.log('user- disconnected');
+   		io.emit("sys-msg", clients[socket.id].name + " left.");
+   	});
+
 	socket.on('usr-msg', function(msg)
 	{
-    	io.emit('usr-msg', msg);
+		if(msg.startsWith("/"))
+		{
+			if(processCommand(socket, msg))
+			{
+				return;
+			}
+		}
+
+    	io.emit('usr-msg', {name:clients[socket.id].name, message:msg.replace(/</g, "&lt;").replace(/>/g, "&gt;"), avatarurl:clients[socket.id].avatarurl});
 	});
 });
 
-http.listen(3000, function()
+function processCommand(socket, msg)
 {
-  console.log('listening on *:3000');
+	if(msg.startsWith("/nick"))
+	{
+		var newName = msg.substring(5);
+		io.emit('sys-msg', clients[socket.id].name + " is now known as " + newName);
+		clients[socket.id].name = newName;
+		return true;
+	}
+	if(msg.startsWith("/youtube"))
+	{
+		return true;
+	}
+	if(msg.startsWith(".system"))
+	{
+		return true;
+	}
+	return false;
+}
+
+http.listen(1234, function()
+{
+  console.log('listening on *:1234');
 });
