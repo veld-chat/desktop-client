46be39e6ccaee63da26de6baa6484d89dfb40a2e
Mike <veldsinkie.mv@gmail.com>
2020-08-08 01:03:40 +0200

    add member caching

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 const httpserver = require('./api/httpserver');
-const httpserverinstance = new httpserver(express, app, http).Listen(1234);
+const httpserverinstance = new httpserver(express, app, http);
+httpserverinstance.Listen(1234);
 const clientmanager = require('./api/clientmanager');
-const clientmanagerinstance = new clientmanager(io).Start();
+const clientmanagerinstance = new clientmanager(io);
+clientmanagerinstance.Start();
