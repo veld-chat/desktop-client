c9adcbf54d1ee86766a55a557d1d63d0c4f62d71
Mike <veldsinkie.mv@gmail.com>
2020-08-10 03:01:35 +0200

    add service worker

--- client/gridsome.config.js
+++ client/gridsome.config.js
@@ -11,7 +11,27 @@ module.exports = {
       use: 'gridsome-plugin-typescript',
     },
     {
-      use: "gridsome-plugin-service-worker",
-    },
+      use: '@allanchain/gridsome-plugin-pwa',
+      options: {
+        manifestOptions: {
+          short_name: 'chat.veld',
+          description: 'zero-data chat application',
+          display: 'standalone',
+          gcm_sender_id: undefined,
+          start_url: '/',
+          categories: ['education'],
+          lang: 'en-GB',
+          dir: 'auto'
+        },
+        appleMobileWebAppStatusBarStyle: 'default',
+        manifestPath: 'manifest.json',
+        icon: 'static/img/logo.png',
+        msTileColor: '#1d1c21',
+        workboxOptions: {
+          cacheId: 'chat-veld',
+          skipWaiting: true
+        }
+      }
+    }
   ]
 }
