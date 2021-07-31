e5d8f941f2fc8cef8fed7a198b0be489572fe3b5
Mike <veldsinkie.mv@gmail.com>
2021-07-31 18:42:04 +0200

    Chore: set membersOpen to true by default

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const IndexPage = () => {
     connect();
   }, []);
 
-  const [membersOpen, setMembersOpen] = React.useState(false);
+  const [membersOpen, setMembersOpen] = React.useState(true);
 
   return (
     <Layout>
